7441a58028f79dc1f7c7b366ad77377b6d2742ea
D055847 <stefan.hengl@sap.com>
2017-11-19 10:03:00 +0100

    handle shift command

--- hw.js
+++ hw.js
@@ -13,6 +13,7 @@ Vue.component('lala', {
             console.log(e.keyCode)
             if ((e.metaKey || e.ctrlKey) && e.keyCode == 13) {
                 console.log("command shift")
+                app.$emit('newField')
             }
             else if (e.keyCode === 13) {
                 e.preventDefault();
@@ -26,16 +27,25 @@ Vue.component('lala', {
       }
 })
 
+function immutablePush(arr, newEntry){
+    return [ ...arr, newEntry ]      
+  }
+
 app = new Vue({
     el: '#app',
     data: {
-        texts: {}
+        texts: {},
+        items: [{id: 1}]
     },
 	created() {
 		this.$on('subsub', function(msg, id){
             console.log('Event from parent component emitted', msg, id)
             this.texts = Object.assign({}, this.texts, {[id]: msg})
-		});
+        });
+        this.$on('newField', function() {
+            console.log("adding new field")
+            this.items = immutablePush(this.items, {id:this.items.length +1})
+        })
 	},
 
     })
